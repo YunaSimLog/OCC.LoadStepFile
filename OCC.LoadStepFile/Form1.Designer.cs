35204070bea40fa7f9b0ad14a4e35376e88eb91c
ynsim <ynsim@solidit.co.kr>
2025-07-23 20:28:52 +0900

    #✨feature: STEP 파일 불러오기 기능 구현
    - 선택 기능 구현
    - 마우스 호버 하이라이트 기능 구현

---
+++ OCC.LoadStepFile/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace OCC.LoadStepFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ribbonPageCategory1 = new DevExpress.XtraBars.Ribbon.RibbonPageCategory();
+            this.ribbonPage2 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.btnLoadSTEPFile = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.btnShowShadingMode = new DevExpress.XtraBars.BarButtonItem();
+            this.btnShowWireMode = new DevExpress.XtraBars.BarButtonItem();
+            this.btnAmbientOcclusion = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPageGroup4 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.barEditColor = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemColorPickEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemColorPickEdit();
+            this.btnAllSelect = new DevExpress.XtraBars.BarButtonItem();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemColorPickEdit1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 150);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1073, 600);
+            this.panel1.TabIndex = 1;
+            this.panel1.SizeChanged += new System.EventHandler(this.panel1_SizeChanged);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // ribbonPageCategory1
+            // 
+            this.ribbonPageCategory1.Name = "ribbonPageCategory1";
+            this.ribbonPageCategory1.Text = "ribbonPageCategory1";
+            // 
+            // ribbonPage2
+            // 
+            this.ribbonPage2.Name = "ribbonPage2";
+            this.ribbonPage2.Text = "ribbonPage2";
+            // 
+            // ribbonPageGroup2
+            // 
+            this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+            this.ribbonPageGroup2.Text = "ribbonPageGroup2";
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1,
+            this.ribbonPageGroup3,
+            this.ribbonPageGroup4});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "ribbonPage1";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnLoadSTEPFile);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "불러오기 도구";
+            // 
+            // btnLoadSTEPFile
+            // 
+            this.btnLoadSTEPFile.Caption = "STEP 파일 불러오기";
+            this.btnLoadSTEPFile.Id = 1;
+            this.btnLoadSTEPFile.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnLoadSTEPFile.ImageOptions.SvgImage")));
+            this.btnLoadSTEPFile.Name = "btnLoadSTEPFile";
+            this.btnLoadSTEPFile.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnLoadSTEPFile_ItemClick);
+            // 
+            // ribbonPageGroup3
+            // 
+            this.ribbonPageGroup3.ItemLinks.Add(this.btnShowShadingMode);
+            this.ribbonPageGroup3.ItemLinks.Add(this.btnShowWireMode);
+            this.ribbonPageGroup3.ItemLinks.Add(this.btnAmbientOcclusion);
+            this.ribbonPageGroup3.ItemLinks.Add(this.barEditColor);
+            this.ribbonPageGroup3.Name = "ribbonPageGroup3";
+            this.ribbonPageGroup3.Text = "표현 도구";
+            // 
+            // btnShowShadingMode
+            // 
+            this.btnShowShadingMode.Caption = "쉐이딩 보기";
+            this.btnShowShadingMode.Id = 3;
+            this.btnShowShadingMode.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnShowShadingMode.ImageOptions.SvgImage")));
+            this.btnShowShadingMode.Name = "btnShowShadingMode";
+            this.btnShowShadingMode.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnShowShadingMode_ItemClick);
+            // 
+            // btnShowWireMode
+            // 
+            this.btnShowWireMode.Caption = "와이어 보기";
+            this.btnShowWireMode.Id = 4;
+            this.btnShowWireMode.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnShowWireMode.ImageOptions.SvgImage")));
+            this.btnShowWireMode.Name = "btnShowWireMode";
+            this.btnShowWireMode.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnShowWireMode_ItemClick);
+            // 
+            // btnAmbientOcclusion
+            // 
+            this.btnAmbientOcclusion.Caption = "엠비언트 오클루젼";
+            this.btnAmbientOcclusion.Id = 5;
+            this.btnAmbientOcclusion.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnAmbientOcclusion.ImageOptions.SvgImage")));
+            this.btnAmbientOcclusion.Name = "btnAmbientOcclusion";
+            this.btnAmbientOcclusion.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnAmbientOcclusion_ItemClick);
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.EmptyAreaImageOptions.ImagePadding = new System.Windows.Forms.Padding(35, 32, 35, 32);
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem,
+            this.ribbonControl1.SearchEditItem,
+            this.btnLoadSTEPFile,
+            this.barButtonItem1,
+            this.btnShowShadingMode,
+            this.btnShowWireMode,
+            this.btnAmbientOcclusion,
+            this.barEditColor,
+            this.btnAllSelect});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ribbonControl1.MaxItemId = 9;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.OptionsMenuMinWidth = 385;
+            this.ribbonControl1.PageCategories.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageCategory[] {
+            this.ribbonPageCategory1});
+            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+            this.ribbonControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemColorPickEdit1});
+            this.ribbonControl1.Size = new System.Drawing.Size(1073, 150);
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "음영 보기1";
+            this.barButtonItem1.Id = 2;
+            this.barButtonItem1.Name = "barButtonItem1";
+            // 
+            // ribbonPageGroup4
+            // 
+            this.ribbonPageGroup4.ItemLinks.Add(this.btnAllSelect);
+            this.ribbonPageGroup4.Name = "ribbonPageGroup4";
+            this.ribbonPageGroup4.Text = "선택";
+            // 
+            // barEditColor
+            // 
+            this.barEditColor.Caption = "모델 색상";
+            this.barEditColor.Edit = this.repositoryItemColorPickEdit1;
+            this.barEditColor.Id = 7;
+            this.barEditColor.Name = "barEditColor";
+            this.barEditColor.EditValueChanged += new System.EventHandler(this.barEditColor_EditValueChanged);
+            // 
+            // repositoryItemColorPickEdit1
+            // 
+            this.repositoryItemColorPickEdit1.AutoHeight = false;
+            this.repositoryItemColorPickEdit1.AutomaticColor = System.Drawing.Color.Black;
+            this.repositoryItemColorPickEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemColorPickEdit1.Name = "repositoryItemColorPickEdit1";
+            // 
+            // btnAllSelect
+            // 
+            this.btnAllSelect.Caption = "전체 선택";
+            this.btnAllSelect.Id = 8;
+            this.btnAllSelect.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btnAllSelect.ImageOptions.SvgImage")));
+            this.btnAllSelect.Name = "btnAllSelect";
+            this.btnAllSelect.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnAllSelect_ItemClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 750);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ribbonControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Ribbon = this.ribbonControl1;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemColorPickEdit1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageCategory ribbonPageCategory1;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage2;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.BarButtonItem btnLoadSTEPFile;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.BarButtonItem btnShowShadingMode;
+        private DevExpress.XtraBars.BarButtonItem btnShowWireMode;
+        private DevExpress.XtraBars.BarButtonItem btnAmbientOcclusion;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup4;
+        private DevExpress.XtraBars.BarEditItem barEditColor;
+        private DevExpress.XtraEditors.Repository.RepositoryItemColorPickEdit repositoryItemColorPickEdit1;
+        private DevExpress.XtraBars.BarButtonItem btnAllSelect;
+    }
+}
+
---
+++ OCC.LoadStepFile/Form1.cs
@@ -0,0 +1,216 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OCC.LoadStepFile
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        #region attributes
+        // --------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// * OpenCASCADE Proxy 객체
+        /// - 초기화는 Form 생성자에서 수행
+        /// </summary>
+        OCCTProxy _occtProxy;
+
+        // --------------------------------------------------------------------------------------------------------------
+        #endregion
+
+        #region private
+
+        /// <summary>
+        /// OCCTProxy 뷰어 초기화
+        /// </summary>
+        /// <returns></returns>
+        private bool InitilizeOCCTProxy()
+        {
+            bool ret = true;
+
+            // #01. OCCTProxy 뷰어 초기화
+            //  - 패널 영역을 OpenCASCADE로 사용할 것 이다.
+            ret &= _occtProxy.InitViewer(panel1.Handle);
+
+            if (!ret)
+            {
+                XtraMessageBox.Show("OpenCASCADE 뷰어 초기화에 실패하였습니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return ret;
+        }
+
+        private bool LoadSTEPFile(string filePath)
+        {
+            bool ret = true;
+
+            // #01. 파일 존재 여부 확인
+            ret &= File.Exists(filePath);
+
+            if (ret)
+            {
+                // #02. OCCTProxy의 TranslateModel을 이용하여 STEP 파일 부르기 수행
+                _occtProxy.ImportStep(filePath);
+
+                // #03. 전체 보기 뷰
+                _occtProxy.ZoomAllView();
+            }
+
+            return ret;
+        }
+
+        #endregion
+
+        #region public
+        /// <summary>
+        /// Form1 생성자
+        /// </summary>
+        public Form1()
+        {
+            // #01. Form 컨트롤 객체 초기화
+            InitializeComponent();
+
+            // #02. OpenCASCADE 를 사용하기 위한 OCCTProxy 객체 초기화
+            _occtProxy = new OCCTProxy();
+        }
+        #endregion
+
+        /// <summary>
+        /// Form1 불러오기 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // #01. OpenCASCADE 초기화 및 패널에 뷰 연결
+            InitilizeOCCTProxy();
+
+            // * STEP 파일 경로
+            string stepFilePath = "../../../Sample/linkrods.step";
+
+            // #01. STEP 파일 불러오기 수행
+            LoadSTEPFile(stepFilePath);
+
+            // * 쉐이딩 모드로 설정
+            _occtProxy.SetShadingMode();
+            
+            // * 선택 색상 적용
+            _occtProxy.SetSeletedStyle(220, 10, 10);
+            
+            // * 하이라이트 색상 적용
+            _occtProxy.SetHighlightStyle(23, 44, 120);
+
+            // * 현재 컬러 가져오기
+            GetCurrentColor();
+        }
+
+        /// <summary>
+        /// Panel1 다시 그리기 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // #01. 뷰 다시 그리기
+            _occtProxy.RedrawView();
+
+            // #02. 뷰 업데이트 하기
+            _occtProxy.UpdateView();
+        }
+
+        /// <summary>
+        /// STEP 파일 불러오기 클릭 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLoadSTEPFile_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "STEP 파일 (*.step, *.stp) | *.step; .stp; | 모든 파일 (.) | .";
+            openFileDialog.Multiselect = false;
+
+            DialogResult dialogResult = openFileDialog.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                // * STEP 파일 경로
+                string stepFilePath = openFileDialog.FileName;
+
+                // #01. STEP 파일 불러오기 수행
+                LoadSTEPFile(stepFilePath);
+            }
+        }
+
+        /// <summary>
+        /// 패널 사이즈 변경 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel1_SizeChanged(object sender, EventArgs e)
+        {
+            // #01. 뷰 업데이트 하기
+            _occtProxy.UpdateView();
+        }
+
+        private void btnShowShadingMode_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            // #01. 쉐이딩 모드로 설정
+            _occtProxy.SetShadingMode();
+        }
+
+        private void btnShowWireMode_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            // #01. 와이어 모드로 설정
+            _occtProxy.SetWireMode();
+        }
+
+        private void btnAmbientOcclusion_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            // #01. 엠비언트 오클루젼 설정 (작업중)
+            _occtProxy.EnableAmbientOcclusion();
+        }
+
+        private bool GetCurrentColor()
+        {
+            bool ret = true;
+
+            int r, g, b;
+
+            r = _occtProxy.GetObjColR();
+            g = _occtProxy.GetObjColG();
+            b = _occtProxy.GetObjColB();
+
+            Color color = Color.FromArgb(r, g, b);
+            barEditColor.EditValue = color;
+
+            return ret;
+        }
+
+        private void barEditColor_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _occtProxy.Select();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            _occtProxy.MoveTo(e.X, e.Y);
+        }
+
+        private void btnAllSelect_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            _occtProxy.SelectAllObject();
+        }
+    }
+}
